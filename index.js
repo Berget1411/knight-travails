6bab69a1f4944f6466990084fd1470c5fd4d28e5
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-11-04 20:00:57 +0100

    add squareNode, update getMoves
    change Getmoves func to work with node argument

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+class SquareNode {
+  constructor(coordinates, predecessor = undefined) {
+    this.coordinates = coordinates;
+    this.predecessor = predecessor;
+  }
+}
+
 // possible changes of coordinates x & y when knight moves
 const KNIGHT_MOVES = [
   [1, 2],
@@ -11,8 +18,28 @@ const KNIGHT_MOVES = [
 ];
 
 // get all possible & allowed moves of knight in start position x & y
-const getMoves = ([x, y]) =>
-  // map adds change in coordinater while filter makes sure the coordinates are not outside the chess board with 8x8
-  KNIGHT_MOVES.map(([a, b]) => [a + x, b + y]).filter(
-    ([a, b]) => a >= 0 && a < 8 && b >= 0 && b < 8,
+const getMoves = (node) => {
+  const [nodeX, nodeY] = node.coordinates;
+  let [predX, predY] = [null, null]; // if no predecessor to node exists
+  if (node.predecessor !== undefined) {
+    [predX, predY] = node.predecessor.coordinates;
+  }
+  return KNIGHT_MOVES.map(([a, b]) => [a + nodeX, b + nodeY]).filter(
+    ([a, b]) =>
+      a >= 0 && a < 8 && b >= 0 && b < 8 && `${a}${b}` !== `${predX}${predY}`,
   );
+};
+// map adds change in coordinater while filter makes sure the coordinates are not outside the chess board with 8x8
+
+//tests below
+const start = [0, 0];
+const second = [1, 2];
+
+const squareStart = new SquareNode(start);
+const squareSecond = new SquareNode(second);
+
+squareSecond.predecessor = squareStart;
+const visited = new Map();
+visited.set(squareStart.coordinates.toString(), getMoves(squareStart));
+visited.set(squareSecond.coordinates.toString(), getMoves(squareSecond));
+console.log(visited);
